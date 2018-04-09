2e01d0075fb78a59c7b6fc3483938d2795e16a65
Austin Concepcion <snazzy2011@gmail.com>
2018-04-09 19:59:49 -0400

    Minor alterations to searchsettings related classes, no true functionality

--- app/src/main/java/edu/valenciacollege/austinconcepcion/petproject/SearchSettingsFragment.java
+++ app/src/main/java/edu/valenciacollege/austinconcepcion/petproject/SearchSettingsFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
 
 
 /**
@@ -22,31 +25,17 @@ public class SearchSettingsFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText priceLowET, priceHighET;
+    private ImageButton dateButton, locationButton;
+    private HorizontalScrollView petHSV;
 
     private OnFragmentInteractionListener mListener;
 
-    public SearchSettingsFragment() {
-        // Required empty public constructor
-    }
+    public SearchSettingsFragment() {    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchSettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchSettingsFragment newInstance(String param1, String param2) {
+    public static SearchSettingsFragment newInstance() {
         SearchSettingsFragment fragment = new SearchSettingsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,8 +44,7 @@ public class SearchSettingsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
