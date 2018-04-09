4fdff68bd6e03da93273be7384193da3978ded27
Austin Concepcion <snazzy2011@gmail.com>
2018-04-09 19:32:12 -0400

    SearchSettings has layout design

--- app/src/main/java/edu/valenciacollege/austinconcepcion/petproject/SearchSettingsFragment.java
+++ app/src/main/java/edu/valenciacollege/austinconcepcion/petproject/SearchSettingsFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SearchSettings.OnFragmentInteractionListener} interface
+ * {@link SearchSettingsFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SearchSettings#newInstance} factory method to
+ * Use the {@link SearchSettingsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SearchSettings extends Fragment {
+public class SearchSettingsFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class SearchSettings extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public SearchSettings() {
+    public SearchSettingsFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class SearchSettings extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchSettings.
+     * @return A new instance of fragment SearchSettingsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SearchSettings newInstance(String param1, String param2) {
-        SearchSettings fragment = new SearchSettings();
+    public static SearchSettingsFragment newInstance(String param1, String param2) {
+        SearchSettingsFragment fragment = new SearchSettingsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
--- app/src/main/java/edu/valenciacollege/austinconcepcion/petproject/TabbedHome.java
+++ app/src/main/java/edu/valenciacollege/austinconcepcion/petproject/TabbedHome.java
@@ -17,14 +17,14 @@ public class TabbedHome extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_pets:
+                    mTextMessage.setText(R.string.title_pets);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_listings:
+                    mTextMessage.setText(R.string.title_listings);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
                     return true;
             }
             return false;
