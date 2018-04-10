c749682de87bc8e09885b4e8df7d72c132d5f0eb
Austin Concepcion <snazzy2011@gmail.com>
2018-04-10 16:04:33 -0400

    Pet model with basic methods created

--- app/src/main/java/edu/valenciacollege/austinconcepcion/petproject/Pets.java
+++ app/src/main/java/edu/valenciacollege/austinconcepcion/petproject/Pets.java
@@ -16,11 +16,70 @@ public class Pets {
      *                       0 - xsmall; 1 - small; 2 - med
      *                       3 - large;  4 - xlarge 5 - xxlarge
      */
-    boolean specialNeeds;
-    String petName, imageName;
+    private boolean specialNeeds;
+    private String petName, imageName;
     /* petName: pet's name
      *
      * imageName: Because we do not use server integration, we need to (temporarily)
      *            store pet pictures locally at drawable/imageName
      */
+
+    public Pets() {}
+
+    public Pets(int id, int type, int sizeClass, boolean specialNeeds, String petName, String imageName) {
+        this.id = id;
+        this.type = type;
+        this.sizeClass = sizeClass;
+        this.specialNeeds = specialNeeds;
+        this.petName = petName;
+        this.imageName = imageName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getSizeClass() {
+        return sizeClass;
+    }
+
+    public void setSizeClass(int sizeClass) {
+        this.sizeClass = sizeClass;
+    }
+
+    public boolean isSpecialNeeds() {
+        return specialNeeds;
+    }
+
+    public void setSpecialNeeds(boolean specialNeeds) {
+        this.specialNeeds = specialNeeds;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
 }
